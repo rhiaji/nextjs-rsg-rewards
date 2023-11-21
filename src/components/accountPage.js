8ff7dbfe587459694e489e26f2c1bf655c04bd4a
rhiaji <mrdavepmariano@gmail.com>
2023-11-21 22:53:59 +0800

    packs

--- src/components/accountPage.js
+++ src/components/accountPage.js
@@ -9,9 +9,70 @@ const SSC = require('sscjs')
 const ssc = new SSC('https://api2.hive-engine.com/rpc')
 
 const AccountPage = () => {
+    const [show, setShow] = useState(true)
     const [data, setData] = useState([])
     const [balance, setBalance] = useState([])
+    const [packs, setPacks] = useState()
     const [user, setUser] = useState('')
+
+    const fetchCards = async () => {
+        let limit = 1000
+        let offset = 0
+        let targetLimit = 3000
+        let allHistory = []
+        let packs = []
+        let hasMoreData = true
+
+        while (hasMoreData) {
+            try {
+                const url = `https://history.hive-engine.com/accountHistory?account=${user}&limit=${limit}&offset=${offset}&symbol=STAR`
+
+                const response = await fetch(url)
+                const data = await response.json()
+
+                data.forEach((element) => {
+                    if (element.operation === 'nft_issueMultiple') {
+                        const card = {
+                            type: element.properties.type,
+                            class: element.properties.class,
+                            stats: element.properties.stats,
+                            timestamp: element.timestamp,
+                            nft: element.nft,
+                            tx: element.transactionId,
+                        }
+                        allHistory.push(card)
+                    }
+                })
+
+                // Update offset for the next request
+                offset += limit
+
+                // Check if the total cards exceed the target limit
+                if (allHistory.length >= targetLimit) {
+                    // Trim excess data beyond the target limit
+                    allHistory = allHistory.slice(0, targetLimit)
+                    console.log(`Reached the target limit of ${targetLimit} cards. Stopping.`)
+                    hasMoreData = false
+                }
+            } catch (error) {
+                console.error('Error:', error)
+                // Handle errors here if needed
+                hasMoreData = false // Stop the loop on error
+            }
+        }
+
+        // Define the batch size
+        const batchSize = 3
+
+        // Loop through the data array in batches
+        for (let i = 0; i < allHistory.length; i += batchSize) {
+            const batch = allHistory.slice(i, i + batchSize)
+            packs.push(batch)
+        }
+
+        console.log(packs)
+        setPacks(packs)
+    }
 
     const fetchData = async (player) => {
         const url = `https://risingstargame.com/playerstats.asp?player=${player}`
@@ -97,7 +158,62 @@ const AccountPage = () => {
 
     return (
         <div className={style.container}>
-            <div className={style.content}>
+            <div className={style.cardContent} style={{ display: show ? 'none' : 'block' }}>
+                <section className={style.header}>
+                    <div>
+                        <p>Your last 3000 pack Openings </p>
+                    </div>
+                    <div>
+                        <button onClick={() => setShow(true)}> Back to account </button>
+                    </div>
+                </section>
+                <section className={style.header}>
+                    <div>Date</div>
+                    <div>Cards</div>
+                </section>
+                <div className={style.transactions}>
+                    {packs &&
+                        packs.map((result) => (
+                            <section className={style.section}>
+                                <div className={style.timestamp}>
+                                    <span>
+                                        <h4>{new Date(result[0].timestamp * 1000).toLocaleString()}</h4>
+                                    </span>
+                                    <span>
+                                        <a href={`https://hivehub.dev/tx/${result[0].tx}`}>Tx: {result[0].tx}</a>
+                                    </span>
+                                    <span>
+                                        <a href={`https://hivehub.dev/tx/${result[1].tx}`}>Tx: {result[1].tx}</a>
+                                    </span>
+                                    <span>
+                                        <a href={`https://hivehub.dev/tx/${result[2].tx}`}>Tx: {result[2].tx}</a>
+                                    </span>
+                                </div>
+                                <div className={style.cards}>
+                                    <div className={style.cardsContent}>
+                                        <span>Type: {result[0].type}</span>
+                                        <span>Class: {result[0].class}</span>
+                                        <span>Stats: {result[0].stats}</span>
+                                        <span>NFT ID: {result[0].nft}</span>
+                                    </div>
+                                    <div className={style.cardsContent}>
+                                        <span>Type: {result[1].type}</span>
+                                        <span>Class: {result[1].class}</span>
+                                        <span>Stats: {result[1].stats}</span>
+                                        <span>NFT ID: {result[1].nft}</span>
+                                    </div>
+                                    <div className={style.cardsContent}>
+                                        <span>Type: {result[2].type}</span>
+                                        <span>Class: {result[2].class}</span>
+                                        <span>Stats: {result[2].stats}</span>
+                                        <span>NFT ID: {result[2].nft}</span>
+                                    </div>
+                                </div>
+                            </section>
+                        ))}
+                </div>
+            </div>
+            <div className={style.content} style={{ display: show ? 'block' : 'none' }}>
                 <section className={style.search}>
                     <div>
                         <p>@{user} risingstar's account</p>
@@ -207,7 +323,14 @@ const AccountPage = () => {
                         </div>
                         <div>
                             <h3>Opened Packs</h3>
-                            <button>Check Cards</button>
+                            <button
+                                onClick={async () => {
+                                    fetchCards()
+                                    setShow(false)
+                                }}
+                            >
+                                Check Cards
+                            </button>
                         </div>
                     </div>
                 </section>
